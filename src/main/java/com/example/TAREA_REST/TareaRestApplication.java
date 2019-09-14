f7107daeee05424045065c60bac61dfb29e84a89
Luis <luisinmedina@gmail.com>
2019-09-14 11:16:40 -0400

    Primer Commit Creacion de la clase contructora Pumakatari

---
+++ src/main/java/com/example/TAREA_REST/TareaRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.TAREA_REST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaRestApplication.class, args);
+	}
+
+}
