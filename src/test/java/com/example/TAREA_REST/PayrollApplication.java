256cdfdb6b9c40167bda346af6ec73b1c6b221d3
Luis <luisinmedina@gmail.com>
2019-09-14 11:22:57 -0400

    Tercer Commit creacion de la Aplicacion

---
+++ src/test/java/com/example/TAREA_REST/PayrollApplication.java
@@ -0,0 +1,12 @@
+package com.example.TAREA_REST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(PayrollApplication.class, args);
+    }
+}
